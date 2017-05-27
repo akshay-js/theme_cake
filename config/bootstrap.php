d951ba28f855c309d7a019afc80e35501a9e787d
Akshay Sharma <akshaya.sharma770@gmail.com>
2017-05-27 11:16:28 +0530

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -231,7 +231,7 @@ Plugin::load('Muffin/Slug',['autoload'=>true]);
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => true]);
+    //Plugin::load('DebugKit', ['bootstrap' => true]);
 }
 
 /**
